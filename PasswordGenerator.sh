354183d28809ee310bcc70b3e8260f37ce7227cb
Sanjit10 <sanjitnepalkhanal@gmail.com>
2024-07-15 10:31:12 +0545

    init: SImple bash projects

---
+++ PasswordGenerator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Simple Password Generator
+
+echo "This is a simple password generator"
+echo "Please enter the length of the password: "
+read PASS_LENGTH
+echo "How many passwords would you like to generate"
+read SEQ_LENGTH
+
+for p in $(seq 1 $SEQ_LENGTH):
+do
+        openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+        # We are using all 48 characters avilable in openssl to genreate a random base64 string
+        # after that we are piping the output to a cut to get a string of user specified length
+done
